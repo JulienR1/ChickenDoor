f002d5e0c5cd04c3180540b0e54351b1e816ef40
Julien <julrousseau20@gmail.com>
2020-08-28 11:27:17 -0400

    Update temp avec wifi

--- ChickenDoor.ino
+++ ChickenDoor.ino
@@ -5,7 +5,7 @@ int upRelayPin = 8;
 int downRelayPin = 9;
 
 int doorMovementDelay = 250;   // in ms
-int updateDelay = 10000;      // 15 mins in ms = 15*60*1000
+int updateDelay = 10000;      // 15 mins in ms = 10*1000
 
 int doorDirection = 0;  // -1: down, 0: up, 1: up
 
@@ -35,17 +35,30 @@ void setPins(){
 void resetDoor(){
   if(!isUp() && !isDown()){
     doorDirection = -1;
+    recordMovementStarted();
   }
 }
 
 void determineDoorDirection(){
   if(doorDirection == 0){
-    if(isDay() && !isUp()){
+    doorDirection = getOnlineDoorDirection();
+  }
+
+  bool daytime = isDay();
+  if(daytime){
+    recordDayTime(daytime);
+  }
+  
+  if(doorDirection == 0){    
+    if(daytime && !isUp()){
       doorDirection = 1;
-    }else if(isNight() && !isDown()){
+      recordMovementStarted();      
+    }else if(!daytime && !isDown()){
       doorDirection = -1;
-    }
+      recordMovementStarted();
+    }    
   }else if((isDown() && doorDirection < 0) || (isUp() && doorDirection > 0)){
+    recordMovementComplete(doorDirection < 0);
     doorDirection = 0;
   }
 }
@@ -75,6 +88,18 @@ bool isDay(){
   return digitalRead(lightSensorPin) == LOW;
 }
 
-bool isNight(){
-  return !isDay();
+int getOnlineDoorDirection(){
+  // https://jrousseau.ca/chickendoor/php/arduino_doorDirection.php
+}
+
+void recordMovementComplete(int doorState){
+  // https://jrousseau.ca/chickendoor/php/arduino_recordMovement.php?doorState=
+}
+
+void recordMovementStarted(){
+ // https://jrousseau.ca/chickendoor/php/arduino_recordMovement.php?doorDirection=
+}
+
+void recordDayTime(bool isDay){
+  // https://jrousseau.ca/chickendoor/php/arduino_daytime.php?daytime=
 }
